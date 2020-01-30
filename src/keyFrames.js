44208aec4d858ae68cb070d639c2f841859fc3fb
bdsqqq <igorbedesqui@gmail.com>
2020-01-30 18:31:54 -0300

    Implementação do componente wrapper das animações

---
+++ src/keyFrames.js
@@ -0,0 +1,8 @@
+
+import styled, { keyframes } from 'styled-components';
+ 
+export const Wobbler = keyframes`
+    0% {transform: translateY(-2px);}
+    50% {transform: translateY(2px);}
+    100% {transform: translateY(-2px);}
+`
