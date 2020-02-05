8e633501959e4bd55b68df34fc4f7f5afcfecd60
bdsqqq <igorbedesqui@gmail.com>
2020-02-04 22:21:57 -0300

    Implementação do contact

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Header from './header'
 import Hero from './hero'
 import Footer from './footer'
 import Folio from './folio'
+import Contact from './contact'
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Folio />
-      <Footer/>
+      <Contact/>
     </>
   );
 }
