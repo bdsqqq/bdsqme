ab5730f09bae5fef07ebf48a2ee50f1db75dcb08
bdsqqq <igorbedesqui@gmail.com>
2020-02-05 16:56:12 -0300

    Implementação dos estilos no formulário

--- src/contact.js
+++ src/contact.js
@@ -8,7 +8,26 @@ function Contact(){
 
     console.log(watch('example')) // watch input value by passing the name of it
 
-
+    const ContatoWrapper = styled.div`
+        position: fixed;
+        left: 0;
+        right: 0;
+        width: 340px;
+        margin: calc(50vh - 220px) auto;
+    `
+    const FormWrapper = styled.div`
+        width: 340px;
+        height: auto;
+        background: #fefefede;
+        border-radius: 8px;
+        box-shadow: 0 0 40px -10px #000;
+        margin: auto;
+        padding: 20px 30px;
+        max-width: calc(100vw - 40px);
+        box-sizing: border-box;
+        font-family: 'Montserrat', sans-serif;
+        position: relative;
+    `
     const FormInputWrapper = styled.p`
         &:before{
             content: attr(name);
@@ -29,28 +48,57 @@ function Contact(){
         font-family: 'Montserrat', sans-serif;
         transition: all .3s;
         border-bottom: 2px solid #bebed2;
-        color: #fefefe;
+        color: #000;
 
         &:focus{
             border-bottom: 2px solid #78788c;
         }
     `
+    const FormButton = styled.input`
+        cursor: pointer;
+
+        text-align: center;
+        color: #0000007c;
+        font-size: 1.2rem;
+        font-family: 'lora';
+
+        padding: .4em;
+        margin: 2em;
+        height: 3rem;
+        width: 100%;
+        background-color: #f9ca24;
+        margin: auto;
+        box-shadow: 0.3rem 0.4rem 0.4rem rgba(0, 0, 0, 0.3);
+        transition: 0.3s ease-in-out;
+        
+        &:hover{
+            opacity: 0.8;
+        }
+        &:disabled{
+            opacity: 0.5;
+        }
+    `
+    const FormAviso = styled.span`
+        font-size: .8em;
+        color: #eb4d4b;
+        opacity: .8;
+    `
     return(
-       
-    <form onSubmit={handleSubmit(onSubmit)}>
-   
-        <input name="example" defaultValue="test" ref={register} />
+    <ContatoWrapper>
+        <FormWrapper>
+            <form onSubmit={handleSubmit(onSubmit)}>
+        
+                <FormInputWrapper name="Nome">
+                    <FormInput name="Nome"  ref={register({ required: true })}/>
+                </FormInputWrapper>
 
-        <input name="exampleRequired" ref={register({ required: true })} />
+                {errors.Nome && <FormAviso>É necessário informar seu nome</FormAviso>}
 
-        {errors.exampleRequired && <span>This field is required</span>}
-      
-        <FormInputWrapper>
-            <FormInput name="Nome" defaultValue="" ref={register}/>
-        </FormInputWrapper>
-
-        <input type="submit" />
-    </form>
+                <FormButton type="submit" />
+            </form>
+        </FormWrapper>
+    </ContatoWrapper>
+    
     );
 }
 
