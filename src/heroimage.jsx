0ef3c55f890ada121b1d9c41d3ed654d8b9fbd5e
bdsqqq <igorbedesqui@gmail.com>
2020-02-11 02:11:06 -0300

    correção de bugs e adição da prop transparencia

--- src/heroimage.jsx
+++ src/heroimage.jsx
@@ -5,23 +5,29 @@ import styled from 'styled-components'
 function HeroImage(props){
     const size = props.size;
     const photoURL = props.photoURL;
+    const transparency = props.transparency;
 
     const HeroImage = styled.div`
         position: absolute;
+        top: 0;
+        left: 0;
         height: ${size}vh;
         width: 100%;
-        z-index: -1;
+        z-index: -20;
 
-        background:linear-gradient(0deg,rgba(0,0,0,0.6),rgba(0,0,0,0.3)),url('${photoURL}');
+        background:linear-gradient(0deg,rgba(0,0,0,${transparency}),rgba(0,0,0,0.3)),url('${photoURL}');
         
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center center;
     `
 
-
+    
     return(
-        <HeroImage></HeroImage>
+        <div>
+            <HeroImage />
+        </div>
+        
     )
 }
 
