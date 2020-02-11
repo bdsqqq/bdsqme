43d63e95bd2fa2aef359188420d0e101e72c30b9
bdsqqq <igorbedesqui@gmail.com>
2020-02-11 02:11:30 -0300

    implementação dos componentes para demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Header from './header'
 import Hero from './hero'
+import HeroImage from './heroimage'
 import Footer from './footer'
 import Folio from './folio'
-import Contact from './contactTest'
+
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <Contact/>
-    </>
-  );
+    return (
+      <>
+        <HeroImage size="100" transparency="0.9" photoURL="https://images.unsplash.com/photo-1529603992250-a55acb77d146?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+        <Header/>
+        <Hero/>
+        <Folio />
+        <Footer/>
+      </>
+    );
 }
 
 export default App;
