766dadf7cd278cd821858b081d0db282f3de6978
bdsqqq <igorbedesqui@gmail.com>
2020-03-11 15:20:24 -0300

    project wherestheiss?? added

--- src/folio.jsx
+++ src/folio.jsx
@@ -53,12 +53,27 @@ function Folio(){
         </Wrapper>
         <CardContainer>
             <Card 
+                projeto={"Onde está a iss??"} 
+                ano={"2020"} 
+                role={"Desenvolvedor Web Fullstack"} 
+                tools={[
+                    {id:1,name:"React "},
+                    {id:2,name:"Fetch API "},
+                    {id:4,name:"React Hooks "},
+                    {id:3,name:"Leaflet "}
+                ]}
+                demo={"https://iss.bdsq.me/"}
+                source={"https://github.com/bdsqqq/react-wherestheiss"}
+                img={"https://i.postimg.cc/6QVmQ85j/wherestheiss.jpg"}
+            />
+            <Card 
                 projeto={"Portfolio em AngularJS"} 
                 ano={"2019"} 
                 role={"Desenvolvedor Web FrontEnd"} 
                 tools={[
                     {id:1,name:"Angular.js "},
-                    {id:2,name:"TypeScript "}
+                    {id:2,name:"TypeScript "},
+                    {id:3,name:"Fetch API "},
                 ]}
                 demo={"https://igorbedesqui.com/"}
                 source={"https://github.com/bdsqqq/bdsq.me_angular"}
