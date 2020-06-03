90ba07e827dba4e4c8fd8c6e678411622be9227b
bdsqqq <igorbedesqui@gmail.com>
2020-06-02 23:56:40 -0300

    implemented router for index page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Header from './header'
-import Hero from './hero'
-import HeroImage from './heroimage'
-import Footer from './footer'
-import Folio from './folio'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import './App.css';
+import Home from './home'
 
 function App() {
     return (
       <>
-        <HeroImage size="100" transparency="0.9" photoURL="https://images.unsplash.com/photo-1529603992250-a55acb77d146?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
-        <Header/>
-        <Hero/>
-        <Folio />
-        <Footer/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
       </>
     );
 }
---
+++ src/home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './header'
+import Hero from './hero'
+import HeroImage from './heroimage'
+import Footer from './footer'
+import Folio from './folio'
+
+import './App.css';
+
+function Home(){
+    
+    return (
+    <>
+      <HeroImage size="100" transparency="0.9" photoURL="https://images.unsplash.com/photo-1529603992250-a55acb77d146?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+      <Header/>
+      <Hero/>
+      <Folio />
+      <Footer/>
+    </>
+  );
+}
+
+export default Home
