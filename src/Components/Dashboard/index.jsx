8fdc8742d1f684f7eb6da4cff2a6db202bfd47b0
bdsqqq <igorbedesqui@gmail.com>
2020-06-18 22:46:41 -0300

    implemented basic form

--- src/Components/Dashboard/index.jsx
+++ src/Components/Dashboard/index.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+import { useForm } from "react-hook-form";
+
+import db from '../../firebase';
+
 
 const Dashboard = () => {
+
+    const [ano, setAno] = useState()
+
+    const { register, handleSubmit, errors, formState } = useForm({
+        mode: "onChange"
+    });
+
+    const onSubmit= data => {
+        setAno(data.ano)
+        console.log(data)
+    }
+
     return(
-        <h1>Hej dö World</h1>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="title">Titulo</label>
+                <input ref={register()} name="title" type="text"/>
+
+                <label htmlFor="ano">Ano</label>
+                <input ref={register()} name="ano" type="text"/>
+                <button type="submit">Enviar</button>
+            </form>
+
+        <h2>{ano}</h2>
+        </div>
     );
 }
 
